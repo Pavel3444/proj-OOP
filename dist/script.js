181708780331ff5a380c88e5109c87d030e9ece0
Pavel3444 <burikov.pavel3444@yandex.ru>
2020-09-17 14:45:31 +0400

    add float block on 3 slide

--- dist/script.js
+++ dist/script.js
@@ -974,13 +974,30 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         this.slideIndex = 1;
       }
 
       if (n < 1) {
-        this.slideIndex = this.slide.length;
+        this.slideIndex = this.slides.length;
       }
+
+      try {
+        this.hanson.style.opacity = '0';
+
+        if (n === 3) {
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (e) {}
 
       this.slides.forEach(function (slide) {
         slide.style.display = "none";
@@ -988,24 +1005,28 @@ function () {
       this.slides[this.slideIndex - 1].style.display = "block";
     }
   }, {
-    key: "plussSlides",
-    value: function plussSlides(n) {
+    key: "plusSlides",
+    value: function plusSlides(n) {
       this.showSlides(this.slideIndex += n);
     }
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch (e) {}
 
       this.btns.forEach(function (item) {
         item.addEventListener('click', function () {
-          _this.plussSlides(1);
+          _this2.plusSlides(1);
         });
         item.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault();
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
         });
       });
       this.showSlides(this.slideIndex);
